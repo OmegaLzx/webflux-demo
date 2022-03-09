ac560d5b6d72dfcdd8509168b0600535a81f1a79
李泽萱 <924900671@qq.com>
2022-03-09 17:05:30 +0800

    init commit

--- src/main/java/pri/omega/webfluxdemo/WebfluxDemoApplication.java
+++ src/main/java/pri/omega/webfluxdemo/WebfluxDemoApplication.java
@@ -2,10 +2,20 @@ package pri.omega.webfluxdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class WebfluxDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebfluxDemoApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routes() {
+        return RouterFunctions.route().GET("/test2",
+                request -> ServerResponse.ok().bodyValue("functional")).build();
     }
 }
